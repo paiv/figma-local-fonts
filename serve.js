b66b772215564930b0ef0df5f574b39d1b498bb8
Pavlo Ivashkov <pavels.code@gmail.com>
2021-03-21 12:44:12 +0200

    :dog:

--- serve.js
+++ serve.js
@@ -129,97 +129,235 @@ function scanfontsjs(roots) {
     // scanfonts.js
     const fs = require('fs')
     const path = require('path')
+    const zlib = require('zlib')
 
     function parse_font_file(fn) {
         let data = fs.readFileSync(fn)
-        const sig = data.readUInt32BE(0)
-        if (sig === 0x74746366) { // ttcf
-            let spec = analyze_font_collection(data)
-            if (spec && spec.length) {
-                return spec
-            }
-            return
+        let spec;
+        switch (data.readUInt32BE(0)) {
+            case 0x74746366: // ttcf
+                spec = analyze_font(data, otc_reader); break
+            case 0x774f4646: // wOFF
+                spec = analyze_font(data, woff_reader); break
+            case 0x774f4632: // wOF2
+                spec = analyze_font(data, woff2_reader); break
+            default:
+                spec = analyze_font(data, otf_reader); break
         }
-        let spec = analyze_font(data)
-        if (spec) {
-            return [spec]
+        return Array.from(spec)
+    }
+
+    function* analyze_font(data, reader) {
+        const tags = new Set(['head', 'name', 'OS/2'])
+        for (let section of reader(data, tags)) {
+            yield extract_attributes(section)
         }
     }
 
-    function analyze_font_collection(data) {
-        const major = data.readUInt16BE(4)
-        const minor = data.readUInt16BE(6)
-        if ((major !== 1 && major !== 2) || (minor !== 0)) { return }
-        const nfonts = data.readUInt32BE(8)
-        let fonts = []
-        for (let i = 0, off = 12; i < nfonts; ++i, off += 4) {
-            const p = data.readUInt32BE(off)
-            let font = analyze_font(data, p)
-            if (font) {
-                fonts.push(font)
+    function extract_attributes(section) {
+        function parse_head(data, attribs) {
+            const major = data.readUInt16BE(0)
+            const minor = data.readUInt16BE(2)
+            if (major !== 1 || minor !== 0) {
+                throw new Error(`Unsupported format version (${major},${minor})`)
             }
-        }
-        return fonts
-    }
-
-    function analyze_font(data, tableOffset=0) {
-        const sig = data.readUInt32BE(tableOffset+0)
-        if (sig !== 0x4F54544F) { // OTTO
-            const major = data.readUInt16BE(tableOffset+0)
-            const minor = data.readUInt16BE(tableOffset+2)
-            if (major !== 1 || minor !== 0) { return }
+            const magic = data.readUInt32BE(12)
+            if (magic !== 0x5F0F3CF5) {
+                throw new Error(`Invalid signature ${magic}`)
+            }
+            const macStyle = data.readUInt16BE(44)
+            attribs['italic'] = ((macStyle & 2) !== 0)
         }
 
-        const ntables = data.readUInt16BE(tableOffset+4)
-        let offsets = new Map()
-        for (let i = 0, off = tableOffset+12; i < ntables; ++i, off += 16) {
-            const tag = data.subarray(off, off+4).toString('utf-8')
-            const p = data.readUInt32BE(off+8)
-            offsets.set(tag, p)
-        }
-        {
-            const p = offsets.get('head')
-            let buf = data.subarray(p, p + 16)
-            const major = buf.readUInt16BE(0)
-            const minor = buf.readUInt16BE(2)
-            if (major !== 1 || minor !== 0) { return }
-            const magic = buf.readUInt32BE(12)
-            if (magic != 0x5F0F3CF5) { return }
-        }
-
-        const head = offsets.get('head')
-        const macStyle = data.readUInt16BE(head+44)
-
-        let attribs = {italic:((macStyle & 2) !== 0)}
-
-        {
+        function parse_name(data, attribs) {
             const nameids = new Map([[1, 'family'], [2, 'style'], [6, 'postscript']])
-            const name = offsets.get('name')
-            const version = data.readUInt16BE(name+0)
-            const nrecs = data.readUInt16BE(name+2)
-            const strings = name + data.readUInt16BE(name+4)
-            for (let off = name+6, i = 0; i < nrecs; ++i, off += 12) {
-                const splat = data.readUInt16BE(off+0)
-                const sspec = data.readUInt16BE(off+2)
-                const slang = data.readUInt16BE(off+4)
+            const version = data.readUInt16BE(0)
+            const nrecs = data.readUInt16BE(2)
+            const strings = data.readUInt16BE(4)
+            for (let off = 6, i = 0; i < nrecs; ++i, off += 12) {
                 const nameid = data.readUInt16BE(off+6)
-                const len = data.readUInt16BE(off+8)
-                const soff = data.readUInt16BE(off+10)
-                const s = decodeString(data.subarray(strings+soff, strings+soff+len), splat, sspec, slang)
                 const sn = nameids.get(nameid)
-                if (sn && s) {
-                    attribs[sn] = s
+                if (sn) {
+                    const splat = data.readUInt16BE(off+0)
+                    const sspec = data.readUInt16BE(off+2)
+                    const slang = data.readUInt16BE(off+4)
+                    const len = data.readUInt16BE(off+8)
+                    const soff = data.readUInt16BE(off+10)
+                    const s = decodeString(data.subarray(strings+soff, strings+soff+len), splat, sspec, slang)
+                    if (s) {
+                        attribs[sn] = s
+                    }
                 }
             }
         }
-        if (offsets.has('OS/2')) {
-            const table = offsets.get('OS/2')
-            const weight = data.readUInt16BE(table+4)
-            const width = data.readUInt16BE(table+6)
-            attribs['weight'] = weight
-            attribs['width'] = width
+
+        function parse_OS_2(data, attribs) {
+            attribs['weight'] = data.readUInt16BE(4)
+            attribs['width'] = data.readUInt16BE(6)
+        }
+
+        let attribs = {}
+        for (let [tag, body] of section) {
+            switch (tag) {
+                case 'head': parse_head(body, attribs); break
+                case 'name': parse_name(body, attribs); break
+                case 'OS/2': parse_OS_2(body, attribs); break
+            }
         }
         return attribs
+    }
+
+    function* otc_reader(data, tags) {
+        const major = data.readUInt16BE(4)
+        const minor = data.readUInt16BE(6)
+        if ((major !== 1 && major !== 2) || (minor !== 0)) {
+            throw new Error(`Unsupported format version (${major},${minor})`)
+        }
+        const nfonts = data.readUInt32BE(8)
+        for (let i = 0, off = 12; i < nfonts; ++i, off += 4) {
+            const p = data.readUInt32BE(off)
+            yield* otf_reader(data, tags, p)
+        }
+    }
+
+    function* otf_reader(data, tags, tableOffset=0) {
+        function* inner() {
+            const sig = data.readUInt32BE(tableOffset+0)
+            if (!(sig === 0x4F54544F || sig === 0x00010000 || sig === 0x74727565)) { // OTTO, 0100, true
+                throw new Error(`Unsupported format ${sig}`)
+            }
+            const ntables = data.readUInt16BE(tableOffset+4)
+            for (let i = 0, off = tableOffset+12; i < ntables; ++i, off += 16) {
+                const tag = data.subarray(off, off+4).toString('utf-8')
+                if (tags.has(tag)) {
+                    const p = data.readUInt32BE(off+8)
+                    const n = data.readUInt32BE(off+12)
+                    const body = data.subarray(p, p+n)
+                    yield [tag, body]
+                }
+            }
+        }
+        yield inner()
+    }
+
+    function* woff_reader(data, tags) {
+        function* inner() {
+            const ntables = data.readUInt16BE(12)
+            for (let i = 0, off = 44; i < ntables; ++i, off += 20) {
+                const tag = data.subarray(off, off+4).toString('utf-8')
+                if (tags.has(tag)) {
+                    const p = data.readUInt32BE(off+4)
+                    const n = data.readUInt32BE(off+8)
+                    const body = zlib_decompress(data.subarray(p, p+n))
+                    yield [tag, body]
+                }
+            }
+        }
+        yield inner()
+    }
+
+    function* woff2_reader(data, tags) {
+        const flavor = data.readUInt32BE(4)
+        const ntables = data.readUInt16BE(12)
+        const compressed_size = data.readUInt32BE(20)
+        const known_tags = new Map([[1, 'head'], [5, 'name'], [6, 'OS/2']])
+        let offsets = []
+        let off = 48
+        let p = 0, n
+        for (let i = 0; i < ntables; ++i) {
+            const flags = data[off]
+            off += 1
+            let tag = flags & 0x3f
+            const trver = flags >> 6
+            const transformed = (tag === 10 || tag === 11) ? (trver !== 3) : (trver !== 0)
+            if (tag === 63) {
+                tag = data.subarray(off, off+4).toString('utf8')
+                off += 4
+            }
+            else {
+                tag = known_tags.get(tag)
+            }
+            [n, off] = read_UIntBase128(data, off)
+            if (transformed) {
+                [n, off] = read_UIntBase128(data, off)
+            }
+            offsets.push([tag, p, n])
+            p += n
+        }
+
+        let directory = [offsets]
+        if (flavor === 0x74746366) { // ttcf
+            off += 4
+            directory = []
+            let nfonts = 0;
+            [nfonts, off] = read_255UInt16(data, off)
+            for (let fi = 0; fi < nfonts; ++fi) {
+                let ntables = 0;
+                [ntables, off] = read_255UInt16(data, off)
+                off += 4
+                let xs = []
+                for (let ti = 0; ti < ntables; ++ti) {
+                    let idx = 0;
+                    [idx, off] = read_255UInt16(data, off)
+                    xs.push(offsets[idx])
+                }
+                directory.push(xs)
+            }
+        }
+
+        const compressed = data.subarray(off, off+compressed_size)
+        data = zlib.brotliDecompressSync(compressed)
+
+        function* entry(offsets) {
+            for (let [tag, p, n] of offsets) {
+                if (tags.has(tag)) {
+                    const body = data.subarray(p, p+n)
+                    yield [tag, body]
+                }
+            }
+        }
+
+        for (let t of directory) {
+            yield entry(t)
+        }
+    }
+
+    function read_255UInt16(data, off) {
+        let n = data[off]
+        switch (n) {
+            case 253:
+                n = (data[off+1] << 8) | data[off+2]
+                off += 3; break
+            case 254:
+                n = 506 + data[off+1]
+                off += 2; break
+            case 255:
+                n = 253 + data[off+1]
+                off += 2; break
+            default:
+                off += 1; break
+        }
+        return [n, off]
+    }
+
+    function read_UIntBase128(data, off) {
+        for (let n = 0, i = 0; i < 5; ++i) {
+            const x = data[off]
+            off += 1
+            n = (n << 7) | (x & 0x7f)
+            if ((x & 0x80) == 0) {
+                return [n, off]
+            }
+        }
+        throw new Error('Invalid UIntBase128 value')
+    }
+
+    function zlib_decompress(data) {
+        try {
+            return zlib.inflateSync(data)
+        }
+        catch {}
+        return data
     }
 
     function decodeString(s, platform, spec, lang) {
@@ -259,14 +397,11 @@ function scanfontsjs(roots) {
     }
 
     function* enumerate_fonts(roots) {
-        for (let fn of glob(roots, /\.(?:ttf|ttc|otf|otc)$/i)) {
-            let spec;
+        for (let fn of glob(roots, /\.(?:ttf|ttc|otf|otc|woff2?)$/i)) {
             try {
-                spec = parse_font_file(fn)
-            } catch(e) { console.error(e) }
-            if (spec) {
+                let spec = parse_font_file(fn)
                 yield [fn, spec]
-            }
+            } catch(e) { console.error(fn); console.error(e) }
         }
     }
 
--- serve.py
+++ serve.py
@@ -6,6 +6,7 @@ import socketserver
 import struct
 import sys
 import traceback
+import zlib
 from pathlib import Path
 from urllib.parse import urlsplit, parse_qs
 
@@ -86,73 +87,193 @@ class RequestHandler (http.server.BaseHTTPRequestHandler):
 def parse_font_file(fn):
     with open(fn, 'rb') as fp:
         data = fp.read()
-    sig, = struct.unpack('>I', data[:4])
-    if sig == 0x74746366:
-        spec = analyze_font_collection(data)
+    sig = data[:4]
+    if sig == b'ttcf':
+        spec = analyze_font(data, otc_reader)
+    elif sig == b'wOFF':
+        spec = analyze_font(data, woff_reader)
+    elif sig == b'wOF2':
+        spec = analyze_font(data, woff2_reader)
     else:
-        spec = [analyze_font(data)]
-    return spec
+        spec = analyze_font(data, otf_reader)
+    return list(spec)
 
 
-def analyze_font_collection(data):
+def analyze_font(data, reader):
+    for section in reader(data, [b'head', b'name', b'OS/2']):
+        yield extract_attributes(section)
+
+
+def extract_attributes(section):
+    attribs = dict()
+    for tag, body in section:
+
+        if tag == b'head':
+            major, minor = struct.unpack('>HH', body[:4])
+            if not ((major == 1) and (minor == 0)):
+                raise Exception(f'Unsupported format version {(major, minor)}')
+
+            sig, = struct.unpack('>I', body[12:16])
+            if sig != 0x5F0F3CF5:
+                raise Exception(f'Invalid signature {sig:08X}')
+
+            mac_style, = struct.unpack('>H', body[44:46])
+            italic = (mac_style & 2) != 0
+            attribs['italic'] = italic
+
+        elif tag == b'name':
+            nameids = {1: 'family', 2: 'style', 6: 'postscript'}
+            version, nrecs, table = struct.unpack('>HHH', body[:6])
+            for i in range(nrecs):
+                off = 6 + i * 12
+                splat, sspec, slang, nameid, slen, soff = struct.unpack('>HHHHHH', body[off:off+12])
+                soff += table
+                sn = nameids.get(nameid)
+                if sn:
+                    s = decode_string(body[soff:soff+slen], splat, sspec, slang)
+                    if s:
+                        attribs[sn] = s
+
+        elif tag == b'OS/2':
+            weight, width = struct.unpack('>HH', body[4:8])
+            attribs['weight'] = weight
+            attribs['width'] = width
+
+    return attribs
+
+
+def otc_reader(data, tags):
     major, minor, nfonts = struct.unpack('>HHI', data[4:12])
     if not ((major == 1 or major == 2) and (minor == 0)):
         raise Exception(f'Unsupported format version {(major, minor)}')
-    def inner(data, i):
+    for i in range(nfonts):
         off = 12 + i * 4
         p, = struct.unpack('>I', data[off:off+4])
-        return analyze_font(data, p)
-    return [inner(data, i) for i in range(nfonts)]
+        yield from otf_reader(data, tags, p)
 
 
-def analyze_font(data, table_offset=0):
-    off = table_offset + 0
-    sig, ntables = struct.unpack('>IH', data[off:off+6])
-    if  sig != 0x4F54544F: # OTTO
-        major, minor = struct.unpack('>HH', data[off:off+4])
-        if not ((major == 1) and (minor == 0)):
-            raise Exception(f'Unsupported format version {(major, minor)}')
+def otf_reader(data, tags, table_offset=0):
+    def inner():
+        off = table_offset + 0
+        sig = data[off:off+4]
+        if sig not in (b'OTTO', b'\x00\x01\x00\x00', b'true'):
+            raise Exception(f'Unsupported format {sig}')
 
-    offsets = dict()
+        off = table_offset + 4
+        ntables, = struct.unpack('>H', data[off:off+2])
+        for i in range(ntables):
+            off = table_offset + 12 + i * 16
+            tag = data[off:off+4]
+            if tag in tags:
+                p,n = struct.unpack('>II', data[off+8:off+16])
+                body = data[p:p+n]
+                yield (tag, body)
+
+    yield inner()
+
+
+def woff_reader(data, tags):
+    def inner():
+        ntables, = struct.unpack('>H', data[12:14])
+        for i in range(ntables):
+            off = 44 + i * 20
+            tag = data[off:off+4]
+            if tag in tags:
+                p,n = struct.unpack('>II', data[off+4:off+12])
+                body = zlib_decompress(data[p:p+n])
+                yield (tag, body)
+
+    yield inner()
+
+
+def woff2_reader(data, tags):
+    import brotli
+    flavor = data[4:8]
+    ntables, = struct.unpack('>H', data[12:14])
+    compressed_size, = struct.unpack('>I', data[20:24])
+    known_tags = {1: b'head', 5: b'name', 6: b'OS/2'}
+    offsets = list()
+    off = 48
+    p = 0
     for i in range(ntables):
-        off = table_offset + 12 + i * 16
-        p, = struct.unpack('>I', data[off+8:off+12])
-        tag = data[off:off+4].decode('utf-8')
-        offsets[tag] = p
+        flags = data[off]
+        off += 1
+        tag = flags & 0x3f
+        trver = flags >> 6
+        transformed = (trver != 3) if (tag == 10 or tag == 11) else (trver != 0)
+        if tag == 63:
+            stag = data[off:off+4]
+            off += 4
+        else:
+            stag = known_tags.get(tag)
+        n, off = read_UIntBase128(data, off)
+        if transformed:
+            n, off = read_UIntBase128(data, off)
+        offsets.append((stag, p, n))
+        p += n
 
-    off = offsets['head']
-    major, minor = struct.unpack('>HH', data[off:off+4])
-    if not ((major == 1) and (minor == 0)):
-        raise Exception(f'Unsupported format version {(major, minor)}')
+    directory = [offsets]
+    if flavor == b'ttcf':
+        off += 4
+        directory = list()
+        nfonts, off = read_255UInt16(data, off)
+        for _ in range(nfonts):
+            ntables, off = read_255UInt16(data, off)
+            flavor = data[off:off+4]
+            off += 4
+            xs = list()
+            for _ in range(ntables):
+                idx, off = read_255UInt16(data, off)
+                xs.append(offsets[idx])
+            directory.append(xs)
 
-    sig, = struct.unpack('>I', data[off+12:off+16])
-    if sig != 0x5F0F3CF5:
-        raise Exception(f'Invalid signature {sig:08X}')
+    compressed = data[off:off+compressed_size]
+    data = brotli.decompress(compressed)
 
-    mac_style, = struct.unpack('>H', data[off+44:off+46])
-    italic = (mac_style & 2) != 0
-    attribs = dict(italic=italic)
+    def entry(offsets):
+        for tag, p, n in offsets:
+            if tag in tags:
+                body = data[p:p+n]
+                yield (tag, body)
 
-    nameids = {1: 'family', 2: 'style', 6: 'postscript'}
-    name = offsets['name']
-    version, nrecs, table = struct.unpack('>HHH', data[name:name+6])
-    for i in range(nrecs):
-        off = name + 6 + i * 12
-        splat, sspec, slang, nameid, slen, soff = struct.unpack('>HHHHHH', data[off:off+12])
-        soff += name + table
-        sn = nameids.get(nameid)
-        if sn:
-            s = decode_string(data[soff:soff+slen], splat, sspec, slang)
-            if s:
-                attribs[sn] = s
+    for t in directory:
+        yield entry(t)
 
-    off = offsets.get('OS/2')
-    if off:
-        weight, width = struct.unpack('>HH', data[off+4:off+8])
-        attribs['weight'] = weight
-        attribs['width'] = width
 
-    return attribs
+def read_255UInt16(data, off):
+    x = data[off]
+    n = x
+    if x == 253:
+        n = (data[off+1] << 8) | data[off+2]
+        off += 3
+    elif x == 254:
+        n = 506 + data[off+1]
+        off += 2
+    elif x == 255:
+        n = 253 + data[off+1]
+        off += 2
+    else:
+        off += 1
+    return n, off
+
+
+def read_UIntBase128(data, off):
+    n = 0
+    for _ in range(5):
+        x = data[off]
+        off += 1
+        n = (n << 7) | (x & 0x7f)
+        if not (x & 0x80):
+            return n, off
+    raise Exception('Invalid UIntBase128 value')
+
+
+def zlib_decompress(data):
+    try:
+        return zlib.decompress(data)
+    except zlib.error:
+        pass
+    return data
 
 
 def decode_string(s, platform, spec, lang):
@@ -179,7 +300,7 @@ def scan_fonts(roots):
                     fringe.append(fn)
 
     def enumerate_fonts(roots):
-        rx = re.compile(r'\.(ttf|ttc|otf|otc)$', re.I)
+        rx = re.compile(r'\.(ttf|ttc|otf|otc|woff2?)$', re.I)
         for fn in glob(roots, rx):
             try:
                 spec = parse_font_file(fn)
